632c33aa6dbfcf58b494e1f82f970afcd5620637
Maria Eugenia Piro <piromariaeugenia@gmail.com>
2020-11-03 16:52:36 -0300

    addArraysForAlert

--- formAlerts.js
+++ formAlerts.js
@@ -1,30 +1,39 @@
+var arrayWrong = [''];
+var arrayInputs = [''];
 //Full Name
 var fullName = document.getElementById('fullName');
 // Alert name
 fullName.addEventListener('blur', checkName);
 fullName.addEventListener('focus', uncheckName);
 function checkName (e) {
+    arrayInputs[0] = ''+e.target.value+'';
     if (e.target.value.length>=6 && e.target.value.includes(' ')) {
+    arrayWrong[0] = ' ';
     } else{
         document.getElementsByClassName('wrong')[0].innerHTML = "<p> Enter valid Name and Last Name</p>";
+        arrayWrong[0] = 'Error: Enter valid Name and Last Name';
     }
 }
 function uncheckName() {
     document.getElementsByClassName('wrong')[0].innerHTML = "<p></p>";
 }
+
 //Writing name
 fullName.addEventListener('keyup',writing);
 function writing(e) {
-    document.getElementById('hello').innerHTML = '<h2> Hello '+e.target.value+'</h2>' ;
+    document.getElementById('hello').innerHTML = '<h2> Hello '+e.target.value+'</h2>' ;    
 }
 //Email
 var email = document.getElementById('email');
 email.addEventListener('blur', checkEmail);
 email.addEventListener('focus', uncheckEmail);
 function checkEmail (e) {
+    arrayInputs[1] = ''+e.target.value+'';
     if (e.target.value.includes('.com') && e.target.value.includes('@')) {
+        arrayWrong[1] = ' ';
     } else{        
         document.getElementsByClassName('wrong')[1].innerHTML = "<p> Enter valid Email</p>";
+        arrayWrong[1] = 'Error: Enter valid Email';
     }
 }
 function uncheckEmail() {
@@ -35,12 +44,18 @@ var password = document.getElementById('password');
 password.addEventListener('blur', checkPassword);
 password.addEventListener('focus', uncheckPassword);
 function checkPassword (e) {
+    arrayInputs[2] = ''+e.target.value+'';
     if (e.target.value.length<6) {
         document.getElementsByClassName('wrong')[2].innerHTML = "<p> Your password is too short </p>";
+        arrayWrong[2] = 'Error: Your password is too short';
     } else if (e.target.value.match('[a-z]')===null) {
         document.getElementsByClassName('wrong')[2].innerHTML = "<p> You have to add letters to your password</p>";
+        arrayWrong[2] = 'Error: You have to add letters to your password';
     } else if (e.target.value.match('[0-9]')===null) {
         document.getElementsByClassName('wrong')[2].innerHTML = "<p> You have to add numbers to your password </p>";
+        arrayWrong[2] = 'Error: ou have to add numbers to your password';
+    } else {
+        arrayWrong[2] = ' ';
     }
 }
 function uncheckPassword() {
@@ -51,9 +66,13 @@ var rPassword = document.getElementById('rPassword');
 rPassword.addEventListener('blur', checkRPassword);
 rPassword.addEventListener('focus', uncheckRPassword);
 function checkRPassword (e) {
+    arrayInputs[3] = ''+e.target.value+'';
     if (e.target.value != password.value) {
         document.getElementsByClassName('wrong')[3].innerHTML = "<p> Password incorrect </p>";
-    }    
+        arrayWrong[3] = 'Error: Password incorrect';
+    } else {
+        arrayWrong[3] = ' ';
+    }
 }
 function uncheckRPassword() {
     document.getElementsByClassName('wrong')[3].innerHTML = "<p></p>";
@@ -63,10 +82,15 @@ var age = document.getElementById('age');
 age.addEventListener('blur', checkAge);
 age.addEventListener('focus', uncheckAge);
 function checkAge (e) {
+    arrayInputs[4] = ''+e.target.value+'';
     if (e.target.value-Math.floor(e.target.value) != 0) {
         document.getElementsByClassName('wrong')[4].innerHTML = "<p> Enter a valid number, not decimal </p>";
+        arrayWrong[4] = 'Error: Enter a valid number, not decimal';
     } else if (e.target.value<18) {
         document.getElementsByClassName('wrong')[4].innerHTML = "<p> Must be over 18 </p>";
+        arrayWrong[4] = 'Error: Must be over 18';
+    } else {
+        arrayWrong[4] = ' ';
     }
 }
 function uncheckAge() {
@@ -76,13 +100,19 @@ function uncheckAge() {
 var tel = document.getElementById('tel');
 tel.addEventListener('blur', checkTel);
 tel.addEventListener('focus', uncheckTel);
-function checkTel (e) {    
+function checkTel (e) {
+    arrayInputs[5] = ''+e.target.value+'';
     if (e.target.value.match('[,()-.]')!=null) {        
         document.getElementsByClassName('wrong')[5].innerHTML = "<p> Do not enter special characters </p>";
+        arrayWrong[5] = 'Error: Do not enter special characters';
     } else if (e.target.value.match('[a-z]')!=null) {
         document.getElementsByClassName('wrong')[5].innerHTML = "<p> Wrtie only numbers </p>";
+        arrayWrong[5] = 'Error: Wrtie only numbers';
     } else if (e.target.value.length<7) {
         document.getElementsByClassName('wrong')[5].innerHTML = "<p> Enter at least 7 numbers </p>";
+        arrayWrong[5] = 'Error: Enter at least 7 numbers';
+    } else {
+        arrayWrong[5] = ' ';
     }
 }
 function uncheckTel() {
@@ -93,14 +123,21 @@ var ad = document.getElementById('ad');
 ad.addEventListener('blur', checkAd);
 ad.addEventListener('focus', uncheckAd);
 function checkAd (e) {
+    arrayInputs[6] = ''+e.target.value+'';
     if (e.target.value.length<5) {
         document.getElementsByClassName('wrong')[6].innerHTML = "<p> You need at least 5 characters </p>";
+        arrayWrong[6] = 'Error: You need at least 5 characters';
     } else if (e.target.value.match('[a-z]')===null) {
         document.getElementsByClassName('wrong')[6].innerHTML = "<p> You have to write the name of the street </p>";
+        arrayWrong[6] = 'Error: You have to write the name of the street';
     } else if (e.target.value.match('[0-9]')===null) {
         document.getElementsByClassName('wrong')[6].innerHTML = "<p> It need a number </p>";
+        arrayWrong[6] = 'Error: It need a number';
     } else if (e.target.value.match(' ')===null) {
         document.getElementsByClassName('wrong')[6].innerHTML = "<p> It's not and address </p>";
+        arrayWrong[6] = "Error: It's not and address";
+    } else {
+        arrayWrong[6] = ' ';
     }
 }
 function uncheckAd() {
@@ -111,8 +148,12 @@ var city = document.getElementById('city');
 city.addEventListener('blur', checkCity);
 city.addEventListener('focus', uncheckCity);
 function checkCity (e) {
+    arrayInputs[7] = ''+e.target.value+'';
     if (e.target.value.length<3) {
         document.getElementsByClassName('wrong')[7].innerHTML = "<p> It's too short </p>";
+        arrayWrong[7] = "Error: It's too short";
+    } else {
+        arrayWrong[7] = ' ';
     }
 }
 function uncheckCity() {
@@ -123,8 +164,12 @@ var zip = document.getElementById('zip');
 zip.addEventListener('blur', checkZip);
 zip.addEventListener('focus', uncheckZip);
 function checkZip (e) {
+    arrayInputs[8] = ''+e.target.value+'';
     if (e.target.value.length<3) {
         document.getElementsByClassName('wrong')[8].innerHTML = "<p> It's too short </p>";
+        arrayWrong[8] = "Error: It's too short";
+    } else {
+        arrayWrong[8] = ' ';
     }
 }
 function uncheckZip() {
@@ -135,35 +180,35 @@ var idn = document.getElementById('idn');
 idn.addEventListener('blur', checkIdn);
 idn.addEventListener('focus', uncheckIdn);
 function checkIdn (e) {
+    arrayInputs[9] = ''+e.target.value+'';
     if (e.target.value.length<7 || e.target.value.length>8) {
         document.getElementsByClassName('wrong')[9].innerHTML = "<p> Must be between 7 and 8 numbers </p>";
+        arrayWrong[9] = 'Error: Must be between 7 and 8 numbers';
+    } else {
+        arrayWrong[9] = ' ';
     }
 }
 function uncheckIdn() {
     document.getElementsByClassName('wrong')[9].innerHTML = "<p></p>";
 }
+console.log(arrayWrong, arrayInputs);
 //SUBMIT
 var label = document.querySelectorAll('label');
 var arrayLabel = Array.from(label).map(element=>element.textContent);
 console.log(arrayLabel);
-var inputs = document.querySelectorAll('input');
-var arrayInputs = Array.from(inputs).map(element=>element.textContent);
-console.log(arrayInputs);
-var wrong = document.querySelectorAll('wrong');
-console.log(wrong);
-var arrayWrong = Array.from(wrong).map(element=>element.textContent);
-console.log(arrayWrong);
 var myArray = [];
-for (var i; arrayLabel.length ;i++) {
-    if (document.getElementsByClassName('wrong')[i] === "<p></p>") {
-        myArray[i] = arrayLabel[i] + arrayInputs[i];
+//myArray[0] = arrayLabel[0].concat(arrayInputs[0]+'Hola');
+for (var i=0; arrayInputs.length-1 ;i++) {
+    if (arrayWrong[i] === ' ') {
+        myArray[i] = arrayLabel[i].concat(arrayInputs[i]);
     } else {
-       myArray[i]= arrayLabel[i] + 'Error: ' + arrayWrong[i];
+       myArray[i]= arrayLabel[i].concat(arrayWrong[i]);
     }
 }
+console.log(myArray);
 //Buttom action
-var myForm = document.getElementById('submit');
+/*var myForm = document.getElementById('submit');
 myForm.addEventListener('click', submitForm);
 function submitForm (e) {
     alert(myArray);
-}
+}*/
